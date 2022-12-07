cb06c552c90a5d6d8354e189e75ace7e0cd7c98a
TiPi <105545170+TiPi-school@users.noreply.github.com>
2022-12-07 18:32:31 +0100

    Navigation Done?

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const mobileLinks = document.querySelector('.mobile-nav-links');
+const mobileLink = document.querySelectorAll('.mobile-nav-link');
+const hamburger = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.closeIcon');
+const menuIcon = document.querySelector('.menuIcon');
+
+function toggleMenu() {
+	if (mobileLinks.classList.contains('showMenu')) {
+		mobileLinks.classList.remove('showMenu');
+		closeIcon.style.display = 'none';
+		menuIcon.style.display = 'block';
+	} else {
+		mobileLinks.classList.add('showMenu');
+		closeIcon.style.display = 'block';
+		menuIcon.style.display = 'none';
+	}
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+mobileLink.forEach(function (menuItem) {
+	menuItem.addEventListener('click', toggleMenu);
+});
