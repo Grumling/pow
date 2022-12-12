60b984d498b8ef10f1a56ba5708a01115965f141
Tiia Pitkänen <tiia1.pitkanen@gmail.com>
2022-12-12 14:36:42 +0100

    back to top btn

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
+// Declaring our variables
 const mobileLinks = document.querySelector('.mobile-nav-links');
 const mobileLink = document.querySelectorAll('.mobile-nav-link');
 const hamburger = document.querySelector('.hamburger');
 const closeIcon = document.querySelector('.close-icon');
 const menuIcon = document.querySelector('.menu-icon');
 
+//We are using an if statement to check if mobileLinks have the showMenu class
+// If it does, we're removing the showMenu class
+//If it doesn't, we're adding the showMenu class
 
 function toggleMenu() {
 	if (mobileLinks.classList.contains('showMenu')) {
@@ -17,12 +21,15 @@ function toggleMenu() {
 	}
 }
 
-
+//Setting closeIcon to display none
 closeIcon.style.display = 'none';
 
+//Adding a click event for hamburger
+//When clicked, it will run the toggleMenu function
 hamburger.addEventListener('click', toggleMenu);
 
-//Looping through
+//Looping through all of the menuItems and adding an click event to each of them
 mobileLink.forEach(function (menuItem) {
 	menuItem.addEventListener('click', toggleMenu);
 });
+
