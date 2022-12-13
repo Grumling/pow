89ec3df5f7b0fa8d94896d2f29fc5bfa7197ac6f
Tiia Pitkänen <tiia1.pitkanen@gmail.com>
2022-12-13 16:17:02 +0100

    Newsletter

--- js/index.js
+++ js/index.js
@@ -29,7 +29,59 @@ closeIcon.style.display = 'none';
 hamburger.addEventListener('click', toggleMenu);
 
 //Looping through all of the menuItems and adding an click event to each of them
-mobileLink.forEach(function (menuItem) {
+mobileLink.forEach((menuItem) => {
 	menuItem.addEventListener('click', toggleMenu);
 });
 
+//NEWSLETTER
+
+//Checking if the email is valid
+const validateEmail = (email) =>
+	email.match(
+		/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+	);
+
+//declaring a const variable
+const newsForm = document.querySelector('#newsForm');
+
+//listening for a submit event on the form
+newsForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	//setting the statusElement.textContent to 'Sending ... ' when pressed submit
+	const statusElement = document.querySelector('#status');
+	statusElement.textContent = 'Sending ... ';
+
+	const email = document.querySelector('.finp');
+
+	//Email validation
+
+	//if email is invalid, adding the alertForm class
+	if (email.value === '' || !validateEmail(email.value)) {
+		email.classList.add('alertForm');
+	} else {
+		email.classList.remove('alertForm');
+	}
+
+	//if email has alertForm class, changes textContent of the status
+	if (email.classList.contains('alertForm')) {
+		statusElement.textContent = 'Check your details again 😊';
+	} else {
+		//changing textContent after 3s
+		setTimeout(() => {
+			const statusElement = document.querySelector('#status');
+			statusElement.textContent = 'Thank you for subscribing! 🎉 ';
+		}, 3000);
+	}
+	//changing textContent after 10s
+	setTimeout(() => {
+		const statusElement = document.querySelector('#status');
+		statusElement.textContent = ' ';
+	}, 10000);
+
+	const contactObj = {
+		email: email.value,
+	};
+
+	console.log(contactObj);
+});
